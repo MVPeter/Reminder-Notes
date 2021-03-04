6d115ef9bf80249c6266a46e7698fb5e9aa9a042
MattV <mvpetersen1@gmail.com>
2021-03-04 16:04:47 -0700

    Updated write to file.

--- server.js
+++ server.js
@@ -29,27 +29,44 @@ app.get("/api/notes", (req, res) => {
 });
 
 app.delete("/api/notes/:id", (req, res) => {
- let returnedID = req.params.id
- console.table(savedNotes)
- for (let i = 0; i < savedNotes.length; i++) {
-     if (returnedID == savedNotes[i].id){
-         savedNotes.splice(i, 1);
-     }
-     
- }
- console.log(savedNotes)
- res.send("ok");    
+    let returnedID = req.params.id
+    console.table(savedNotes)
+    for (let i = 0; i < savedNotes.length; i++) {
+        if (returnedID == savedNotes[i].id) {
+            savedNotes.splice(i, 1);
+        }
+
+    }
+    console.log(savedNotes)
+    fs.writeFile(savedNotes, jsonContent, utf8, function (err) {
+        if (err) {
+            console.log("error writing to JSON file");
+            return console.log(err);
+        }
+        console.log("JSON updated");
+    })
+    res.send("ok");
 })
 
 app.post('/api/notes', (req, res) => {
     let returnNote = req.body;
     noteID = noteID + 1;
     returnNote.id = noteID;
-    
     savedNotes.push(returnNote);
+
     console.log(returnNote);
-    res.send("ok");
-})
+    console.log(savedNotes);
+
+    const jsonSavedNotes = JSON.stringify(savedNotes);
+    fs.writeFile("./db/db.json", jsonSavedNotes, 'utf8', function (err) {
+        if (err) {
+            console.log("error writing to JSON file");
+            return console.log(err);
+        }
+        console.log("JSON updated");
+        res.send("ok");
+    })
+});
 
 app.listen(PORT, function () {
     console.log("App listening on PORT http://localhost:" + PORT);
