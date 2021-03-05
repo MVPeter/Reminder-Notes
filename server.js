e774272f24b0539e378cfb0e0735c98b45ebb293
MattV <mvpetersen1@gmail.com>
2021-03-04 17:24:09 -0700

    fixed ID in JSON to be unique.  Updated notes.

--- server.js
+++ server.js
@@ -2,70 +2,76 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const PORT = 3033;
-// const HTMLdir = path.join(__dirname, "public");
-// const CSSdir = path.join(HTMLdir, "assets/css")
 
-
-
-var app = express();
+const app = express();
 const savedNotes = require('./db/db.json');
 app.use(express.static("public"))
 app.use(express.json())
 
 var noteID = 0;
 
-
-
+//root GET route
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+//Notes GET route
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
+//Notes API GET route
 app.get("/api/notes", (req, res) => {
     res.json(savedNotes);
 });
 
+//Delet API route
 app.delete("/api/notes/:id", (req, res) => {
     let returnedID = req.params.id
     console.table(savedNotes)
+
     for (let i = 0; i < savedNotes.length; i++) {
         if (returnedID == savedNotes[i].id) {
             savedNotes.splice(i, 1);
         }
-
     }
-    console.log(savedNotes)
-    fs.writeFile(savedNotes, jsonContent, utf8, function (err) {
+    console.table(savedNotes)
+
+    const jsonSavedNotes = JSON.stringify(savedNotes);
+    fs.writeFile("./db/db.json", jsonSavedNotes, function (err) {
         if (err) {
             console.log("error writing to JSON file");
             return console.log(err);
         }
         console.log("JSON updated");
-    })
+
+    });
     res.send("ok");
 })
 
+//POST API route
 app.post('/api/notes', (req, res) => {
     let returnNote = req.body;
-    noteID = noteID + 1;
+    for (let i = 0; i < savedNotes.length; i++) {
+        if (savedNotes[i].id != i) {
+            noteID = i
+        }
+    };
     returnNote.id = noteID;
     savedNotes.push(returnNote);
 
-    console.log(returnNote);
-    console.log(savedNotes);
+    // console.log(returnNote);
+    console.table(savedNotes);
 
     const jsonSavedNotes = JSON.stringify(savedNotes);
-    fs.writeFile("./db/db.json", jsonSavedNotes, 'utf8', function (err) {
+    fs.writeFile("./db/db.json", jsonSavedNotes, function (err) {
         if (err) {
             console.log("error writing to JSON file");
             return console.log(err);
         }
         console.log("JSON updated");
         res.send("ok");
-    })
+    });
 });
 
 app.listen(PORT, function () {
